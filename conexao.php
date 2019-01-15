d05fdd1c989d408dc7b012ce289220608fc15f7d
Victoria Schoene <44248430+LordViic@users.noreply.github.com>
2019-01-15 18:20:01 -0200

    Banco de dados online e ativo
    efetuado as correçoes no banco de dado e funcionando direito com o servido do heroku.

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 //estamos usando um BD qualquer só para testar conexão enquanto não resolvemos o do site real.
 function getConnection()
 {
-    $link= mysqli_connect("localhost","root","","usuarios");
+    $link= mysqli_connect("jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","n398cf960pg77487","gia0q309w2dl5v9r","ibzunnradjfds7kg");
 
      mysqli_set_charset($link,"utf8");
 

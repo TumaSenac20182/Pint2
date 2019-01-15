10809ec13bba1e7dcb90fe5f0984a0debdec5c97
TECNICO DE INFORMÁTICA 2018.2 <51121742018.2@senacrjedu.local>
2019-01-15 19:46:29 -0200

    Corrreção

--- conexao.php
+++ conexao.php
@@ -1,22 +1,21 @@
 <?php
 
-# constantes
-  define("SERVER", array(
-    "host" => "localhost",
-    "user" => "root",
-    "pass" => "",
-    "dbname" => "sga",
-    "port" => "3306"
-  ));
 
-  function getConnection() {
-    $link = mysqli_connect(SERVER['host'], SERVER['user'], SERVER['pass'], SERVER['dbname'], SERVER['port']);
-    mysqli_set_charset($link, 'utf8');
+define("__SERVER__","jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+define("__USER__","n398cf960pg77487");
+define("__PASS__","gia0q309w2dl5v9r");
+define("__DB__","ibzunnradjfds7kg");
+define("__PORT__",3306);
+
+function getConnection()
+{
+    $link= mysqli_connect(__SERVER__,__USER__,__PASS__,__DB__,__PORT__);
+
+     mysqli_set_charset($link,"utf8");
+
     return $link;
-  }
-
-  if(!getConnection()) {// EOL - End Of Line (Final da linha)
-    echo 'Falha ao abrir a conexão, erro sob o número: ' . mysqli_errno() . PHP_EOL;
-    echo 'A mensagem do erro: ' . mysqli_error() . PHP_EOL;
-    exit;
 }
+ if(!getConnection())
+ {
+   echo "Erro ao conectar".mysqli_error().PHP_EOL;
+ }
---
+++ insert.php
@@ -0,0 +1,13 @@
+<?php
+require "crud.php";
+
+if($_POST)
+{
+  $nome=$_POST['txtNome'];
+  $contato=$_POST['txtTelefone'];
+  $email=$_POST['txtEmail'];
+
+  create($nome, $contato, $email);
+
+  //header('location:');
+}

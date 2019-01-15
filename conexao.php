3aa880a15819c3551b960a92222de77ef1bf650f
Gustavo <>
2019-01-15 18:34:31 -0200

    BANCO DE DADOS
    efetuado a mudança da forma de se conectar ao banco agora ao inves de dos dados estarem diratamentte na variavel agora os mesmo estão dentro de constantes.

--- conexao.php
+++ conexao.php
@@ -1,8 +1,15 @@
 <?php
-//estamos usando um BD qualquer só para testar conexão enquanto não resolvemos o do site real.
+
+
+define("__SERVER__","jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+define("__USER__","n398cf960pg77487");
+define("__PASS__","gia0q309w2dl5v9r");
+define("__DB__","ibzunnradjfds7kg");
+define("__PORT__",3306);
+
 function getConnection()
 {
-    $link= mysqli_connect("jsk3f4rbvp8ayd7w.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","n398cf960pg77487","gia0q309w2dl5v9r","ibzunnradjfds7kg");
+    $link= mysqli_connect(__SERVER__,__USER__,__PASS__,__DB__,__PORT__);
 
      mysqli_set_charset($link,"utf8");
 
